bbb529fc4e326b3e213d2818ef8aacd1841c48c3
Anurag Ajwani <anurag.ajwani@onfido.com>
2021-05-11 19:02:55 +0100

    Changed expected response length to 160

--- Sources/NFCPassportReader/TagReader.swift
+++ Sources/NFCPassportReader/TagReader.swift
@@ -52,7 +52,7 @@ public class TagReader {
     func doInternalAuthentication( challenge: [UInt8], completed: @escaping (ResponseAPDU?, NFCPassportReaderError?)->() ) {
         let randNonce = Data(challenge)
         
-        let cmd = NFCISO7816APDU(instructionClass: 00, instructionCode: 0x88, p1Parameter: 0, p2Parameter: 0, data: randNonce, expectedResponseLength: 256)
+        let cmd = NFCISO7816APDU(instructionClass: 00, instructionCode: 0x88, p1Parameter: 0, p2Parameter: 0, data: randNonce, expectedResponseLength: 160)
 
         send( cmd: cmd, completed: completed )
     }
